c38e152d1f69517751a33be08edfc03724cba759
tsubusuka <51908493+tsubusuka@users.noreply.github.com>
2024-05-27 01:58:53 +0900

    2024/05/26分

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('dateForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
     const dateInput = document.getElementById('dateInput').value;
-    const formattedDate = dateInput.split('-').join('-');  // YYYY-MM-DD 形式
+    const formattedDate = dateInput.split('-').join('-');  // YYYY-MM-DD
     const relativeUrl = `./diary/${formattedDate}.html`;
     
     fetch(relativeUrl)
@@ -10,11 +10,11 @@ document.getElementById('dateForm').addEventListener('submit', function(event) {
             if (response.ok) {
                 window.location.href = relativeUrl;
             } else {
-                document.getElementById('message').textContent = "その日付の日記は存在しません";
+                document.getElementById('message').textContent = "その日の日記はありません";
             }
         })
         .catch(error => {
             console.error('Error fetching the diary page:', error);
-            document.getElementById('message').textContent = "エラーが発生しました";
+            document.getElementById('message').textContent = "エラー発生しますた";
         });
 });
